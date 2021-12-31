35bd074798268dcab047c36c34736c7af8d77d58
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2022-01-01 04:39:43 +0800

    Update __init__.py

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -208,7 +208,7 @@ if SPAMWATCH is None:
 else:
     spamwtc = spamwatch.Client(SPAMWATCH)
 
-REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=false)
 try:
     REDIS.ping()
     LOGGER.info("[Zeldris] Your redis server is now alive!")
